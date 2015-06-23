af05c0aa7f734dc788270b774190e0929ca44355
PanXiaoCheng <pan330125@outlook.com>
2015-06-23 22:37:51 +0800

    Revert "qq登陆"

--- app/src/main/java/com/mrpan/callmeup/ui/MainActivity.java
+++ app/src/main/java/com/mrpan/callmeup/ui/MainActivity.java
@@ -1,31 +1,19 @@
 package com.mrpan.callmeup.ui;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.mrpan.callmeup.R;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
-    private Button btLogin;
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btLogin = (Button)findViewById(R.id.login);
-        btLogin.setOnClickListener(this);
-    }
-    public void LoginActivity(){
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
     }
 
     @Override
@@ -48,16 +36,5 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.login:
-                LoginActivity();
-                break;
-            default:
-                break;
-        }
     }
 }
