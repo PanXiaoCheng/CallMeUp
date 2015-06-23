a8e26f56db43b9a7702f22a4972e40c1a03f9ebf
Mr.wang <136633878@qq.com>
2015-06-23 22:28:23 +0800

    添加了登录activity，实现了QQ授权登录和邀请好友，还加了一个Util,用于下载QQ头像

--- app/src/main/java/com/mrpan/callmeup/ui/MainActivity.java
+++ app/src/main/java/com/mrpan/callmeup/ui/MainActivity.java
@@ -1,24 +1,31 @@
 package com.mrpan.callmeup.ui;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.mrpan.callmeup.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //sssss
-        Toast.makeText(this, "sssss", Toast.LENGTH_LONG).show();
-        //sss
+        btLogin = (Button)findViewById(R.id.login);
+        btLogin.setOnClickListener(this);
+    }
+    public void LoginActivity(){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -41,5 +48,16 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                LoginActivity();
+                break;
+            default:
+                break;
+        }
     }
 }
